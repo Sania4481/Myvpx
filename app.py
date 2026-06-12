d0372ae0f8f28f0098f604f14d30bd1c8915ee75
Sania4481 <sania.5677272@gmail.com>
2026-06-12 07:02:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ except ImportError:
 
 app = Flask(__name__)
 
-TIKTOK_USERNAME = "@nongthituyet00"
+TIKTOK_USERNAME = "@ganji_live_8"
 
 # ══════════════════════════════════════════
 # GLOBAL STATE
